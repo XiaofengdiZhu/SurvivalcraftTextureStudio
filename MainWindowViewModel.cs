93c00529f461835c57a9f6f782a88254da71ebad
销锋镝铸 <523084467@qq.com>
2020-11-22 21:31:21 +0800

    解决一个重复加载材质的bug

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -15,10 +15,11 @@ namespace SurvivalcraftTextureStudio
             {
                 new Page("HomePageTitle", new Home(), PackIconKind.Home),
                 new Page("UISettingsPageTitle", new PaletteSelector() { DataContext = new PaletteSelectorViewModel() },PackIconKind.Theme){ VerticalScrollBarVisibilityRequirement = ScrollBarVisibility.Auto},
-                new Page("BlocksPageTitle", new BlocksPage() { DataContext = new BlocksPageViewModel() },PackIconKind.CubeOutline){MarginRequirement=new System.Windows.Thickness(0) },
+                new Page("BlocksPageTitle", new BlocksPage(),PackIconKind.CubeOutline){MarginRequirement=new System.Windows.Thickness(0) },
             };
             Version version = Assembly.GetExecutingAssembly().GetName().Version;
             VersionText = "v" + version.Major + "." + version.Minor + (version.Build > 0 ? ("." + version.Build) : "") + (version.Revision > 0 ? " beta" + (version.Revision > 1 ? (" " + version.Revision) : "") : "");
+            System.Diagnostics.Debug.WriteLine("主界面VM加载完成");
         }
 
         public Page[] Pages { get; }
