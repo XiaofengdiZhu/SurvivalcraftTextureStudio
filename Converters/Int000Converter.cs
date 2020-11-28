fee6210c804558c37508f571ddc963b226067fd4
销锋镝铸 <523084467@qq.com>
2020-11-28 18:14:53 +0800

    序号不足3位时补0

---
+++ Converters/Int000Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Documents;
+
+namespace SurvivalcraftTextureStudio
+{
+    public class Int000Converter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int? input = value as int?;
+            if (input.HasValue)
+            {
+                string output="";
+                int length = input.Value.ToString().Length;
+                for (int i = 3; i>length ; i--)
+                {
+                    output += '0';
+                }
+                return output;
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return System.Windows.Data.Binding.DoNothing;
+        }
+    }
+}
