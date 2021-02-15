647d135e58a2eb588d5c535a405c1d10165319dc
销锋镝铸 <523084467@qq.com>
2021-02-15 23:44:46 +0800

    首页基本完善

--- Pages/Home.xaml.cs
+++ Pages/Home.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace SurvivalcraftTextureStudio
+﻿using System.Diagnostics;
+
+namespace SurvivalcraftTextureStudio
 {
     public partial class Home : System.Windows.Controls.UserControl
     {
@@ -13,6 +15,40 @@
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             BlocksPageViewModel.BPVM.ImportBlocksTextureBySelectingFile();
+        }
+
+        private void EmailButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //Process.Start(new ProcessStartInfo("cmd", $"/c start mailto:523084467@qq.com") { CreateNoWindow = true });
+            OpenLink("mailto:523084467@qq.com");
+        }
+
+        private void DonateButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenLink("https://github.com/XiaofengdiZhu/SurvivalcraftTextureStudio/blob/main/CONTRIBUTING.md");
+            //Process.Start(new ProcessStartInfo("cmd", $"/c start https://github.com/XiaofengdiZhu/SurvivalcraftTextureStudio/blob/main/CONTRIBUTING.md") { CreateNoWindow = true });
+        }
+        public void OpenLink(string url)
+        {
+            url = url.Replace("&", "^&");
+            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+        }
+        private void WatchChip_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenLink("https://github.com/XiaofengdiZhu/SurvivalcraftTextureStudio/watchers");
+        }
+        private void StarChip_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenLink("https://github.com/XiaofengdiZhu/SurvivalcraftTextureStudio/stargazers");
+        }
+        private void ForkChip_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenLink("https://github.com/XiaofengdiZhu/SurvivalcraftTextureStudio/network/members");
+        }
+
+        private void GithubButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenLink("https://github.com/XiaofengdiZhu/SurvivalcraftTextureStudio/");
         }
     }
 }
